e859a02d54d88496df935633e7ac55f962091d33
Christian Wiggert <christian_wiggert@gmx.de>
2010-05-11 23:08:04 +0200

    Removed some old code

--- lib/thief/freebase/etl.rb
+++ lib/thief/freebase/etl.rb
@@ -6,19 +6,27 @@ module Thief
   module Freebase
     NEWNAME = 'person.tsv'
     FILENAME = File.expand_path(File.join(File.dirname(__FILE__), NEWNAME))
-    BASEURL = "http://toolserver.org/~sk/pd/output_pd_export.zip"
-    EXTURL = ""
-    
+    BASEURL = "http://download.freebase.com/datadumps/"
+    EXTURL = "/browse/people/#{NEWNAME}"
+
     class ETL < Thief::ETL
       
       def self.download_file!
-       # get correct URL
-
+        # TODO: catch all errors!
+        
+        # get correct URL
+        puts 'searching for correct URL'
+        document = open(BASEURL).read
+        m = document.match(/.*href=\"\/datadumps\/(.*?)\/browse\".*/m)
+        if (m.captures.size != 1) 
+          return false
+        end
         # download file
         puts 'downloading file'
         writeOut = open(FILENAME, 'wb')
-        writeOut.write(open(URL).read)
+        writeOut.write(open(BASEURL + m.captures[0] + EXTURL).read)
         writeOut.close
+        return true
       end
       
       def self.fetch(arguments)
@@ -28,14 +36,25 @@ module Thief
         end
         file = File.new(FILENAME, "r")
         counter = 1
+        first = true
+        attrs = [:name, :external_id, :date_of_birth, :place_of_birth, :nationality,
+          :religion, :gender, :meta_web_user_s, :parents, :children, :eployment_history, 
+          :signature, :spouse_s, :sibling_s, :weight_kg, :height_meters, :education, 
+          :profession, :quotations, :places_lived, :ethnicity, :quotationsbook_id, 
+          :age, :tvrage_id]
         while (line = file.gets)
-          c = 1
+          if (first)
+            first = false
+            next
+          end
+          c = 0 # was 1
           person = Person.new
           # split every line and map to the person object
           for part in line.split(/\t/) do
-            case c
-              
-            end 
+            if (c == 24)
+              break
+            end
+            person.send("#{attrs[c]}=", part)
             c += 1
           end
           person.save!
