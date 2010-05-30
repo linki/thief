20d18283bf558ee70e1b77131658f3d616cd9646
Christian Wiggert <christian_wiggert@gmx.de>
2010-05-30 15:26:24 +0200

    bla

--- lib/thief/wikipedia/person.rb
+++ lib/thief/wikipedia/person.rb
@@ -7,13 +7,13 @@ module Thief
 
       property :id, Serial   # An auto-increment integer key
     
-      property :name, String
-      property :alternative_names, String
-      property :short_description, String
-      property :date_of_birth, String
-      property :place_of_birth, String
-      property :date_of_death, String
-      property :place_of_death, String
+      property :name, String # Magellan, Ferdinand
+      property :alternative_names, String # Magalhães, Fernão de (Portuguese); Magallanes, Fernando de (Spanish)
+      property :short_description, String # Sea explorer
+      property :date_of_birth, String # 1480
+      property :place_of_birth, String # Sabrose, Portugal
+      property :date_of_death, String # 27 April 1521
+      property :place_of_death, String # Mactan Island, Cebu, Philippines
     end
   end
 end
